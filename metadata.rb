3be9cc034b235c984a66b50c20918b9173eba09f
Matt Kulka <mkulka@parchment.com>
2018-05-09 10:21:13 -0700

    relax poise-python version dependency

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'matt@lqx.net'
 license          'MIT'
 description      'Procures Let\'s Encrypt SSL certificates for Route 53-hosted domains'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.5'
+version          '1.0.6'
 
 supports     'ubuntu' if respond_to?(:supports)
 chef_version '>= 12'
@@ -14,5 +14,5 @@ chef_version '>= 12'
 issues_url 'https://github.com/mattlqx/cookbook-letsencryptaws/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/mattlqx/cookbook-letsencryptaws' if respond_to?(:source_url)
 
-depends 'poise-python', '~> 1.6.0'
+depends 'poise-python', '~> 1.6'
 depends 'remote_file_s3', '~> 1.0.5'
