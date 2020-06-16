e52703910e90aaa8bf40f42558b4aeb9a5b659c4
Matt Kulka <mkulka@parchment.com>
2020-06-16 08:29:37 -0700

    add knife.rb to please knife for supermarket share

---
+++ .github/knife.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+log_level                :info
+log_location             STDOUT
+node_name                'mkulka'
+cookbook_path            ["#{ENV['GITHUB_WORKSPACE']}/.."]
+cookbook_copyright       'Matt Kulka'
+cookbook_email           'matt@lqx.net'
+cookbook_license         'MIT'
